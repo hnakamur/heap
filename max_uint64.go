a404eba8d477f8b5669524a1d6ccb841f1ad39fa
Hiroaki Nakamura <hnakamur@gmail.com>
2020-01-12 18:22:15 +0900

    Add MinUint64 and MaxUint64

---
+++ max_uint64.go
@@ -0,0 +1,104 @@
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package heap
+
+// MaxUint64 is a heap for getting the maximum uint64 value.
+type MaxUint64 []uint64
+
+// Init establishes the heap invariants required by the other routines in this package.
+// Init is idempotent with respect to the heap invariants
+// and may be called whenever the heap invariants may have been invalidated.
+// The complexity is O(n) where n = len(*h).
+func (h *MaxUint64) Init() {
+	// heapify
+	n := h.length()
+	for i := n/2 - 1; i >= 0; i-- {
+		h.down(i, n)
+	}
+}
+
+// Push pushes the element x onto the heap.
+// The complexity is O(log n) where n = len(*h).
+func (h *MaxUint64) Push(x uint64) {
+	h.push(x)
+	h.up(h.length() - 1)
+}
+
+// Pop removes and returns the maximum element from the heap.
+// The complexity is O(log n) where n = len(*h).
+// Pop is equivalent to Remove(h, 0).
+func (h *MaxUint64) Pop() uint64 {
+	n := h.length() - 1
+	h.swap(0, n)
+	h.down(0, n)
+	return h.pop()
+}
+
+// Remove removes and returns the element at index i from the heap.
+// The complexity is O(log n) where n = len(*h).
+func (h *MaxUint64) Remove(i int) uint64 {
+	n := h.length() - 1
+	if n != i {
+		h.swap(i, n)
+		if !h.down(i, n) {
+			h.up(i)
+		}
+	}
+	return h.pop()
+}
+
+// Fix re-establishes the heap ordering after the element at index i has changed its value.
+// Changing the value of the element at index i and then calling Fix is equivalent to,
+// but less expensive than, calling Remove(h, i) followed by a Push of the new value.
+// The complexity is O(log n) where n = len(*h).
+func (h *MaxUint64) Fix(i int) {
+	if !h.down(i, h.length()) {
+		h.up(i)
+	}
+}
+
+func (h *MaxUint64) up(j int) {
+	for {
+		i := (j - 1) / 2 // parent
+		if i == j || !h.less(j, i) {
+			break
+		}
+		h.swap(i, j)
+		j = i
+	}
+}
+
+func (h *MaxUint64) down(i0, n int) bool {
+	i := i0
+	for {
+		j1 := 2*i + 1
+		if j1 >= n || j1 < 0 { // j1 < 0 after int overflow
+			break
+		}
+		j := j1 // left child
+		if j2 := j1 + 1; j2 < n && h.less(j2, j1) {
+			j = j2 // = 2*i + 2  // right child
+		}
+		if !h.less(j, i) {
+			break
+		}
+		h.swap(i, j)
+		i = j
+	}
+	return i > i0
+}
+
+func (h MaxUint64) length() int        { return len(h) }
+func (h MaxUint64) less(i, j int) bool { return h[i] > h[j] }
+func (h MaxUint64) swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *MaxUint64) push(x uint64) {
+	*h = append(*h, x)
+}
+
+func (h *MaxUint64) pop() (x uint64) {
+	*h, x = (*h)[:h.length()-1], (*h)[h.length()-1]
+	return
+}
