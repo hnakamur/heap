87001fd28301680c0568eb531dd0635a13190c1b
Hiroaki Nakamura <hnakamur@gmail.com>
2020-01-12 18:30:07 +0900

    Add MinInt64 and MaxInt64

---
+++ README.md
@@ -0,0 +1,8 @@
+heap
+====
+
+Package heap is a Go module which is copied from the standard library container/heap
+and modified for concrete types (string, int64, and uint64).
+
+This package also provides the maximum version of heap for getting the maximum value
+from the heap.
--- doc.go
+++ doc.go
@@ -1,12 +1,13 @@
-// Package heap provides heap operations for string and uint64.
+// Package heap provides heap operations for string, int64, and uint64.
 // A heap is a tree with the property that each node is the
 // minimum-valued node in its subtree.
 //
 // The minimum element in the tree is the root, at index 0.
 //
 // This package is copied from the standard library container/heap
-// and modified for concrete types such as string and uint64.
+// and modified for concrete types such as string.
 //
-// Package heap also provides structs MaxStr and MaxUint64 for
-// maximum versions of heap.
+// Package heap also provides structs MaxStr, MaxInt64, and MaxUint64
+// for maximum versions of heap.
+//
 package heap
